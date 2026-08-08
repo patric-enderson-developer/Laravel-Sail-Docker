262625028b1b4611aa9a2967a840123490a9623b
patricchubaca <patric_job@outlook.com>
2026-08-08 15:12:14 -0300

    Feat Model :: Subscribe

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscriber extends Model
 {
-    //
+      protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'company',
+        'position',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
 }
