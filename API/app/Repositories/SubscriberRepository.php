deba288d7abe2fd24af36adae40d73d17e4da25f
patricchubaca <patric_job@outlook.com>
2026-08-10 20:51:26 -0300

    Feat :: SubscriberRepository

--- API/app/Repositories/SubscriberRepository.php
+++ API/app/Repositories/SubscriberRepository.php
@@ -13,12 +13,12 @@ class SubscriberRepository
         return Subscriber::all();
     }
 
-  public function create(array $data): Subscriber
+     public function create(array $data): Subscriber
     {
         return Subscriber::create($data);
     }
     public function findByEmail(string $email): ?Subscriber
-        {
-            return Subscriber::where('email', $email)->first();
-        }
+    {
+        return Subscriber::where('email', $email)->first();
+    }
 }
