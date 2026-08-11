a1cf7b72d94f179b3eff8cc6ad307a60ac4989e4
patricchubaca <patric_job@outlook.com>
2026-08-11 07:07:06 -0300

    Feat :: Posts table migration

--- Adm-Blog/database/migrations/2026_08_11_093550_create_posts_table.php
+++ Adm-Blog/database/migrations/2026_08_11_093550_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->boolean('is_published')->default(false);
             $table->timestamps();
         });
     }
